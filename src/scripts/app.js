44cb96babd56ec30cfda4f35b9683f9567a8c6e4
MrGeseR <geser.nikolaev@gmail.com>
2016-08-25 14:36:21 +0300

    Slider finished

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,5 +4,32 @@ var col = 0;
 
 for(let i=1;i<12;i++) {
     $('.slider').append('<div class="slider__element"><img alt="" src="img/'+i+'.jpg" id="'+i+'"></div>');
-
 }
+var sliding = function(){
+    var slides = $(".slider").children(".slider__element");
+    var width = $(".slider").width();
+    var i = slides.length;
+    var offset = i * width;
+    i--;
+    $(".slider").css('width',offset);
+    offset = 0;
+    $(".wrapper__rightArrow").click(function() {
+        if (offset<width*i) {
+            offset += width;
+            $(".slider").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+        } else {
+            offset = 0;
+            $(".slider").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+        }
+    });
+    $(".wrapper__leftArrow").click(function(){
+        if (offset > 0) {
+            offset -= width;
+            $(".slider").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+        } else {
+            offset = width*i;
+            $(".slider").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+        }
+    });
+};
+sliding();
